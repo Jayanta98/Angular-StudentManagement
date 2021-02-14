02b2377f9c70b6d67ab2c34215388479cc476a81
gdsaikrishna <gdsaikrishna@gmail.com>
2021-02-14 13:09:03 +0530

    models for student

---
+++ src/app/models/student.ts
@@ -0,0 +1,53 @@
+export class StudentRegister{
+    //when student data to be register
+    public firstName: string;
+    public middleName: string;
+    public lastName: string;
+    public fatherName: string;
+    public motherName: string;
+    public mobileNo: string;
+    public emailId: string;
+    public aadhaarNo: string;
+    public dateOfBirth: Date;
+    public resAddress: string;
+    public classNo: number;
+    public scholarship: number;
+    public bag: string;
+    public module: string;
+    public idCard: string;
+    public medium: string;
+    public lastSchool: string;
+    public category: string;
+}
+export class StudentSaved{
+    //when fetching student
+    public registrationNo: number;
+    public firstName: string;
+    public middleName: string;
+    public lastName: string;
+    public fatherName: string;
+    public motherName: string;
+    public mobileNo: string;
+    public emailId: string;
+    public aadhaarNo: string;
+    public dateOfBirth: Date;
+    public resAddress: string;
+    public classNo: number;
+    public scholarship: number;
+    public bag: string;
+    public module: string;
+    public idCard: string;
+    public medium: string;
+    public lastSchool: string;
+    public registrationDate: Date;
+    public rollNo: number;
+    public category: string;
+    public amountPaid: number;
+}
+
+export class StudentList{
+    //when student data to be fetched
+    public studentList: StudentSaved[];
+    public statusCode:string;
+    public statusMessage:string;
+}
