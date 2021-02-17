6d132a9f9c0bbb44e0ecd03e3bf9b17791531edf
Jayanta <jg.16u10555@btech.nitdgp.ac.in>
2021-02-17 19:03:42 +0530

    exam cell

--- src/app/admin/examcell/examcell.component.ts
+++ src/app/admin/examcell/examcell.component.ts
@@ -12,4 +12,51 @@ export class ExamcellComponent implements OnInit {
   ngOnInit(): void {
   }
 
+   on11thmarksCard: any;
+    on11thMarksCardSelection(event) {
+    this.on11thmarksCard = event.target.files[0];
+  }
+  onClassEleventhSubmit(){
+      let formData: FormData = new FormData();
+       formData.append('marksCard11', this.on11thmarksCard);
+       alert(JSON.stringify(formData));
+  }
+
+
+
+   on12thmarksCard: any;
+    on12thMarksCardSelection(event) {
+    this.on12thmarksCard = event.target.files[0];
+  }
+  onClassTwelveSubmit(){
+      let formData: FormData = new FormData();
+       formData.append('marksCard12', this.on12thmarksCard);
+       alert(JSON.stringify(formData));
+  }
+
+
+   TargetBatchmarksCard: any;
+    onTargetBatchMarksCardSelection(event) {
+    this.TargetBatchmarksCard = event.target.files[0];
+  }
+  onClassTargetSubmit(){
+      let formData: FormData = new FormData();
+       formData.append('marksCard', this.TargetBatchmarksCard);
+       alert(JSON.stringify(formData));
+  }
+
+
+
+  studentRegNo:number;
+  trackMarksByRegiNumber(){
+    alert(this.studentRegNo);
+
+  }
+
+  BatchName:any;
+  trackMarksByBatchName(){
+    alert(this.BatchName)
+
+  }
+
 }
