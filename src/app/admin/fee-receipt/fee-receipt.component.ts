62d9c842f20b13eaaa83c98f865ffb8ae3340e3f
gdsaikrishna <gdsaikrishna@gmail.com>
2021-03-12 18:18:50 +0530

    payment receipt

--- src/app/admin/fee-receipt/fee-receipt.component.ts
+++ src/app/admin/fee-receipt/fee-receipt.component.ts
@@ -9,12 +9,15 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class FeeReceiptComponent implements OnInit {
 
-  payment: AccountDto
+  payment: AccountDto = new AccountDto()
 
   constructor(private activatedRoute:ActivatedRoute,private router:Router) { 
     this.payment = <AccountDto>this.router.getCurrentNavigation().extras.state
     console.log(this.payment)
   }
+  paymentGST = 0.09*this.payment.amount
+  paymentAmount = 0.82*this.payment.amount
+  
 
   ngOnInit(): void {
   }
